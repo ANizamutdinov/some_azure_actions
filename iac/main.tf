a922296fe985461e1ce6309dd07a3a1e31eeab99
Artur Nizamutdinov <artur.nizamutdinov@skuvault.com>
2021-04-12 11:13:01 +0500

    set subnet addresses

--- iac/main.tf
+++ iac/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["172.19.0.0/27"]
+  address_space       = ["172.19.0.0/24"]
   name                = join("-", ["vnet", local.name_template])
 }
 
@@ -30,6 +30,7 @@ resource "azurerm_subnet" "snet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   name                 = join("-", ["snet", local.name_template])
+  address_prefixes     = ["172.19.0.32/27"]
 }
 
 resource "azurerm_public_ip" "pip" {
